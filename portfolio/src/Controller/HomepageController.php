f91906072c7358305447eaa8716319f09cb53482
KevinBouris <bourisville.k@outlook.com>
2021-02-16 12:54:32 +0100

    navbar responsive ok

--- portfolio/src/Controller/HomepageController.php
+++ portfolio/src/Controller/HomepageController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomepageController extends AbstractController
 {
     /**
-     * @Route("/", name="homepage")
+     * @Route("/", name="home")
      */
     public function index(): Response
     {
